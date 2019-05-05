c2ecb0052e8c77bcc0cc6d3173b13663da088dd0
Metaphysiker <s.raess@me.com>
2019-05-05 08:44:50 +0200

    added validations

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,2 +1,13 @@
 class Application < ApplicationRecord
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :street, presence: true
+  validates :city, presence: true
+  validates :plz, presence: true
+  validates :email, presence: true
+  validates :telefon, presence: true
+  validates :payment, presence: true
+  validates :motivation, presence: true
+  validates :expectation, presence: true
+  validates :education, presence: true
 end
